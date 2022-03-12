6a662aab3ff5a4fc5d280c2a8d760d87537deaec
esteban <esteban1009@naver.com>
2022-03-12 09:42:21 +0900

    Initial commit

---
+++ src/main/java/com/test/propertyCommuity/PropertyCommuityApplication.java
@@ -0,0 +1,13 @@
+package com.test.propertyCommuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyCommuityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyCommuityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/propertyCommuity/PropertyCommuityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.propertyCommuity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyCommuityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
